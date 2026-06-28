a9bef8646a75048af25ae083eb176a293c51ce3a
lviniciosfs <lviniciosferreirasilva@gmail.com>
2026-06-28 15:18:56 +0100

    JSX params

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
 import React from 'react'; // Adicione esta linha no topo do ficheiro
 
 export default function App() {
+  const surname = 'Lucas'
+  const forename = 'Silva'
   return (
-    <>
-      <h1>Hedy Lamarr's Todos</h1>
-      <img 
-        src="https://i.imgur.com/yXOvdOSs.jpg" 
-        alt="Hedy Lamarr" 
-        className="photo"
-      />
-      <ul>
-        <li>Invent new traffic lights</li>
-        <li>Rehearse a movie scene</li>
-        <li>Improve spectrum technology</li>
-      </ul>
-    </>
+    <><h1>Hello, {surname}!</h1>
+    <p>Second {forename}</p></>
   );
 }
