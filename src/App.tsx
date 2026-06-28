16d578a3b0bdfc46488a39d273430924996f0cf4
lviniciosfs <lviniciosferreirasilva@gmail.com>
2026-06-28 19:07:09 +0000

    Conditional Rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import React from 'react'; // Adicione esta linha no topo do ficheiro
-
-export default function NumberList() {
-  const numbers = [1,2,3,4,5]
-
-  const listItems = numbers.map(i => 
-    <li key={i}>
-      Number: {i}
-    </li>
-  )
-
-  return (
-    <ul>{listItems}</ul>
-  )
+interface WeatherInfoProps {
+  temperature: number; // Use 'string' se a temperatura vier com a unidade (ex: "25°C")
 }
 
+export default function WeatherInfo( {temperature}: WeatherInfoProps) {
+
+  
+  let weatherMessage;
+  if (temperature > 30) {
+    weatherMessage = <p>It's hot outside!</p>;
+  } else {
+    weatherMessage = <p>It's cold, wear a jacket.</p>;
+  }
+  
+  return (
+    <div>
+      {weatherMessage}
+      
+      {/* Method 2: Ternary operator */}
+      <p>{temperature > 25 ? 'Too warm' : 'Comfortable'}</p>
+      
+      {/* Method 3: Logical AND */}
+      {temperature > 32 && <p>Warning: Extreme heat!</p>}
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import NumberList from './App.tsx'
+import WeatherInfo from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <NumberList />
+    <WeatherInfo temperature={35}/>
   </StrictMode>,
 )
