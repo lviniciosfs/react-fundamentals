990b4958589e75d8065b369a3989cd673aa0315a
lviniciosfs <lviniciosferreirasilva@gmail.com>
2026-06-28 17:52:22 +0100

    Rendering Lists fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import React from 'react'; // Adicione esta linha no topo do ficheiro
 export default function NumberList() {
   const numbers = [1,2,3,4,5]
 
-  const listItems = numbers.map(i => {
+  const listItems = numbers.map(i => 
     <li key={i}>
       Number: {i}
     </li>
-  })
+  )
 
   return (
     <ul>{listItems}</ul>
